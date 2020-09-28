ebfe9ef0ed07eb403fc2069be89e1ba7940fb705
Yuta <y.sugiyama54@gmail.com>
2020-09-28 23:48:27 +0900

    セクション3

---
+++ pages/mypage.vue
@@ -0,0 +1,6 @@
+<template>
+    <section class="container">
+        <h1>My Page</h1>
+        <router-link to="/">Top Page</router-link>
+    </section>
+</template>
---
+++ pages/price.vue
@@ -0,0 +1,6 @@
+<template>
+    <section class="container">
+        <h1>Price Page</h1>
+        <router-link to="/">Top Page</router-link>
+    </section>
+</template>
